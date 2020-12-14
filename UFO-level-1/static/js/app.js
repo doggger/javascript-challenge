cbe88898cdcbd2ab3156f892a18de3882a2a589e
unknown <natekellyrd@gmail.com>
2020-12-14 17:33:14 -0500

    final commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,11 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// YOUR CODE HERE!\
+
+// USES D3 TO ASSIGN ELEMENT OF HTML TO VARIABLES
 var tbody = d3.select("tbody");
 var form = d3.select("#form");
 var button = d3.select("#filter-btn");
 
+
+// CREATES INITIAL TABLE WITH FULL DATA
 data.forEach((sighting) => {
     var row = tbody.append("tr");
     Object.entries(sighting).forEach(([key, value]) => {
@@ -14,19 +18,25 @@ data.forEach((sighting) => {
     });
 });
 
+
+// SINGLE FUNCTION FOR SEARCH AND RE-SEARCH
 function runEnter() {
+    //PREVENT RELOAD OF PAGE
     d3.event.preventDefault();
+    //ASSIGN USER INPUT TO A VARIABLE
     usrdate = d3.select("#datetime").property("value");
     console.log(usrdate)
     if(usrdate) {
         var filteredData = tableData.filter(sitng => sitng.datetime === usrdate);
+        // CLEARS TABLE PRIOR TO ADDING DATA SUBSET
         tbody.html("");
+        // RETURNS STRING IF NO DATA IN USER SEARCH
         if (filteredData.length === 0) {
             var row = tbody.append("tr");
             var cell = row.append("td")
             cell.text("No sightings found in specified date range.")
-
         } else {
+             //IF USER SEARCH DATA NOT BLANK THEN CREATES NEW TABLE
             filteredData.forEach((sighting) => {
                 row = tbody.append("tr");
                 Object.entries(sighting).forEach(([key, value]) => {
@@ -35,6 +45,7 @@ function runEnter() {
                 });
             });
         }
+     // IF USER INPUT IS BLANK RELOADS FULL DATA TABLE
     } else {
         tbody.html("");
         data.forEach((sighting) => {
@@ -47,6 +58,7 @@ function runEnter() {
     }
 }
 
+// RUNS SEARCH FUNCTION ON ENTER OR CLICK
 form.on("submit", runEnter);
 button.on("click", runEnter);
 
